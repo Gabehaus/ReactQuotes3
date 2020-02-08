45a2b6f9d457fdeb148a579279d98f17ec8fad8c
Gabriel Hauschildt <54124906+Gabehaus@users.noreply.github.com>
2020-02-07 17:06:06 -0800

    Updates

---
+++ precache-manifest.9241b224e0af6629c86012e6a5d4e5d4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20d7a132b0bdb6fd20b240b0fb07e746",
+    "url": "/ReactQuotes3/index.html"
+  },
+  {
+    "revision": "b46094023ed5535ecf78",
+    "url": "/ReactQuotes3/static/css/main.96e37c58.chunk.css"
+  },
+  {
+    "revision": "b91c6878515d754b2a92",
+    "url": "/ReactQuotes3/static/js/2.5795906c.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/ReactQuotes3/static/js/2.5795906c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b46094023ed5535ecf78",
+    "url": "/ReactQuotes3/static/js/main.956aa7df.chunk.js"
+  },
+  {
+    "revision": "7909de22f010bd167265",
+    "url": "/ReactQuotes3/static/js/runtime-main.32df2c1a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ReactQuotes3/static/media/logo.5d5d9eef.svg"
+  }
+]);
